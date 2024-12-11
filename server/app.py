bf5251b76e2c44bebd7e71c999dba90b3aa2fe78
Tristan Tritch <ttmeep101@hotmail.com>
2024-12-11 15:07:23 -0600

    ADD: we are so talented

--- server/app.py
+++ server/app.py
@@ -14,13 +14,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = db.session.execute(db.select(Message)).scalars()
+        list_messages = [message.to_dict() for message in messages]
+        return make_response(list_messages)
+    elif request.method == 'POST':
+        data = request.json
+        new_message = Message(body=data['body'], username=data['username'])
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    if request.method == 'PATCH':
+        message = db.session.execute(db.select(Message).filter_by(id=id)).scalar_one()
+        data = request.get_json() if request.is_json else request.form.to_dict()
+        for attr, value in data.items():
+            if hasattr(message, attr):
+                setattr(message, attr, value)
+        db.session.commit()
+        return make_response(message.to_dict(),202)
+    if request.method == 'DELETE':
+        message = db.session.execute(db.select(Message).filter_by(id=id)).scalar_one()
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+        return make_response(response_body)
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +13,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.datetime(2024, 12, 11))
+    updated_at = db.Column(db.DateTime, default=datetime.datetime(2024, 12, 11))
